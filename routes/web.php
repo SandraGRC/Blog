12b35d00faa67d6ea067b58dc90f76d712a999af
SandraGRC <34062134+SandraGRC@users.noreply.github.com>
2021-04-28 15:13:45 -0300

    "28/04/2021-teste"

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 }); 
 
-Route::get('users/{id}/{valor?}', function ($id,$valor = null) {
-    if($valor){
-        return "usuário e o seu id é $id e o seu valor é $valor";
-    }else{
-        return "usuário e o seu id é $id";
-    }
-});
-
-Route::get('users', function () {
-    return "usuário";
+Route::get('testa', function () {
+    return "ok";
 });
